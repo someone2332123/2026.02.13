fd5d80bd6315edc6ff1e2b634d7e99e7af07662c
someone2332123 <danieltoth787pc@gmail.com>
2026-02-13 10:46:04 +0100

    v3

--- tetelek.py
+++ tetelek.py
@@ -1,13 +1,23 @@
 import random
 #2-es feladat
 
-def lista_keszites():
-    szamok = [int]
-    for i in range (20):
-        veletlen = random.randint(50, 70)
+def lista_keszites(hossz, mettol, meddig):
+    szamok: list[int] = []
+    for i in range (hossz):
+        veletlen = random.randint(mettol, meddig)
         szamok.append(veletlen)
     return szamok
 
+def megszamlalas(szamok: list[int]):
+    db = 0
+    for i in range(len(szamok)):
+        if szamok[i] < 60:
+            db += 1
+    return db
+
+
+
 def teljes():
-    szamok = lista_keszites()
+    szamok = lista_keszites(20, 50, 70)
     print(szamok)
+    print(f"A szintet teljesítők száma: {megszamlalas(szamok)}")
